7593edabfd53e59343fdc206ef800fbcf5b6980a
Luis <104567400+Surdavow@users.noreply.github.com>
2024-01-13 14:55:21 -0800

    Progress on player controller
    So far there should be a simple animation player setup. Right now I'm still working on improving the player component so that it can sprint too.

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -1,42 +1,77 @@
 using Godot;
+using Godot.Collections;
 using System;
+using System.Diagnostics;
+using System.Reflection.Metadata;
 
 public partial class Player : CharacterBody3D
 {
-	public const float Speed = 5.0f;
-	public const float JumpVelocity = 4.5f;
+    public const float Speed = 3f;
+    public const float JumpVelocity = 4.5f;
+    [Export]
+    public float sensitivity_horizontal = 0.5f;
+    [Export]
+    public float sensitivity_vertical = 0.5f;
+    public Node3D camera_mount_thirdperson;
+    public AnimationPlayer animation_player;
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+    // Get the gravity from the project settings to be synced with RigidBody nodes.
+    public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+    
+    public override void _Ready()
+    {
+        Input.MouseMode = Input.MouseModeEnum.Captured;
+		camera_mount_thirdperson = GetNode<Node3D>("camera_mount_thirdperson");        
+		animation_player = GetNode<AnimationPlayer>("Visuals/alpha/AnimationPlayer");
+    }
 
-	public override void _PhysicsProcess(double delta)
-	{
-		Vector3 velocity = Velocity;
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseMotion mouseMotionEvent)
+        {
+            RotateY(Mathf.DegToRad(-mouseMotionEvent.Relative.X * sensitivity_horizontal));
+            camera_mount_thirdperson.RotateX(Mathf.DegToRad(-mouseMotionEvent.Relative.Y * sensitivity_vertical));
+        }
+    }
 
-		// Add the gravity.
-		if (!IsOnFloor())
-			velocity.Y -= gravity * (float)delta;
+    public override void _PhysicsProcess(double delta)
+    {
+        Vector3 velocity = Velocity;
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-			velocity.Y = JumpVelocity;
+        // Add the gravity.
+        if (!IsOnFloor())
+            velocity.Y -= gravity * (float)delta;
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-		if (direction != Vector3.Zero)
-		{
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
-		}
+        // Handle Jump.
+        if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+            velocity.Y = JumpVelocity;
 
-		Velocity = velocity;
-		MoveAndSlide();
-	}
+        // Get the input direction and handle the movement/deceleration.
+        // As good practice, you should replace UI actions with custom gameplay actions.
+        Vector2 inputDir = Input.GetVector("move left", "move right", "move forward", "move backward");
+        Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+        if (direction != Vector3.Zero)
+        {
+            if (animation_player.CurrentAnimation != "walking")
+            {
+                animation_player.Play("walk");
+            }
+
+            velocity.X = direction.X * Speed;
+            velocity.Z = direction.Z * Speed;
+        }
+        else
+        {
+            if (animation_player.CurrentAnimation != "idle")
+            {
+                animation_player.Play("idle");
+            }
+
+            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+            velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+        }
+
+        Velocity = velocity;
+        MoveAndSlide();
+    }
 }
